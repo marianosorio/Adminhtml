c5ac77ba176cbc8c148c19653695cc53823803db
marianosorio <149555450+marianosorio@users.noreply.github.com>
2024-04-19 10:50:36 -0600

    Update script.js

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+//Enlazar páginas
+function getLogin() {
+    window.location.href = "Login.html";
+}
+
+function getPrincipal() {
+    window.location.href = "Principal.html";
+  }
+
+  function getInfodemandado() {
+    window.location.href = "Infodemandado.html";
+  }
+ 
+  function getInfodemandante() {
+    window.location.href = "Infodemandante.html";
+  }
+
+  function getInfopolicia() {
+    window.location.href = "Infopolicia.html";
+  }
+
+  function getInfotestigo() {
+    window.location.href = "Infotestigo.html";
+  }
