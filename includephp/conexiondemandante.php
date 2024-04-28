815d3906d77a1b456d56cdef8977941ecad00b55
marianosorio <149555450+marianosorio@users.noreply.github.com>
2024-04-28 14:53:23 -0600

    Update conexiondemandante.php

--- includephp/conexiondemandante.php
+++ includephp/conexiondemandante.php
@@ -17,20 +17,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $correo = $_POST['usuario'];
     $contraseña = $_POST['contrasena'];
 
-    $query = ("INSERT INTO demandados(nombre, apellido, casa, calle, ciudad, departamento, id, sexo, genero, numero, delitos)
-    VALUES ('_REQUEST[nombre1]', '_REQUEST[apellido1]', '_REQUEST[casa1]', '_REQUEST[calle1]', '_REQUEST[ciudad1]', '_REQUEST[departamento1]'
-    '_REQUEST[id1]', '_REQUEST[sexo1]', '_REQUEST[genero1]', '_REQUEST[numero], '_REQUEST[delitos1]'')");
+    $query = "INSERT INTO demandados(nombre, apellido, casa, calle, ciudad, departamento, id, sexo, genero, numero, delitos) 
+              VALUES ('" . pg_escape_string($_REQUEST['nombre1']) . "', 
+                      '" . pg_escape_string($_REQUEST['apellido1']) . "', 
+                      '" . pg_escape_string($_REQUEST['casa1']) . "', 
+                      '" . pg_escape_string($_REQUEST['calle1']) . "', 
+                      '" . pg_escape_string($_REQUEST['ciudad1']) . "', 
+                      '" . pg_escape_string($_REQUEST['departamento1']) . "', 
+                      '" . pg_escape_string($_REQUEST['id1']) . "', 
+                      '" . pg_escape_string($_REQUEST['sexo1']) . "', 
+                      '" . pg_escape_string($_REQUEST['genero1']) . "', 
+                      '" . pg_escape_string($_REQUEST['numero']) . "', 
+                      '" . pg_escape_string($_REQUEST['delitos1']) . "')";
 
     $consulta = pg_query($conexion, $query);
-        if ($consulta) {
-            header("Location: Infotestigo.html"); // Redirigir a la página principal
-            exit;
-        } else {
-            echo 'Error al insertar datos.';
-        }
+    if ($consulta) {
+        header("Location: Infotestigo.html"); // Redirigir a la página principal
+        exit;
+    } else {
+        echo 'Error al insertar datos.';
+    }
 }
-
-
 
 pg_close($conexion);
 ?>
